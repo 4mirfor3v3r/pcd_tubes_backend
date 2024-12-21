2edb07661c9962354779369d68e09805e4d61bca
ahmad_amirudin <ahmadamirudin901@gmail.com>
2024-12-21 19:34:45 +0700

    Initial commit

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import os
+
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="info", workers=1)
+    uvicorn.run("main:app", host="0.0.0.0", port=os.environ.get("PORT", 8000), reload=False, log_level="info", workers=1)
